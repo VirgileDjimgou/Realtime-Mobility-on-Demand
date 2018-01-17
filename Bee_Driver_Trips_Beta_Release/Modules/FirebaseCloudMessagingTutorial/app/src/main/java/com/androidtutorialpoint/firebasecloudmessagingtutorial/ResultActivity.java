b31d5d3f925ef6534ec9b024f8fbd32d0bf6c8a4
chichikolon <djimgoupat@yahoo.fr>
2018-01-17 14:12:38 +0100

    Fcm GCM

---
+++ Bee_Driver_Trips_Beta_Release/Modules/FirebaseCloudMessagingTutorial/app/src/main/java/com/androidtutorialpoint/firebasecloudmessagingtutorial/ResultActivity.java
@@ -0,0 +1,19 @@
+package com.androidtutorialpoint.firebasecloudmessagingtutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView textView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView1);
+        textView.setText("Welcome to the Result Activity");
+
+    }
+}
